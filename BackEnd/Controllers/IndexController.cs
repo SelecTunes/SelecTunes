4d6f2ec44efaad197cd73e9c711effd6ee1dc4a5
Alexander Young <stdin@iastate.edu>
2020-01-16 22:42:48 -0600

    Make It Slimmer

---
+++ BackEnd/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cs309server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<String>> Index()
+        {
+            return new List<String> { "" };
+        }
+    }
+}
--- BackEnd/Startup.cs
+++ BackEnd/Startup.cs
@@ -23,7 +23,7 @@ namespace cs309server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,12 +35,10 @@ namespace cs309server
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -48,9 +46,7 @@ namespace cs309server
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
