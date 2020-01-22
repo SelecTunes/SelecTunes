e4543a7830d9cb6e5c7a421f4ffcd3b6f1bc3163
Alexander Young <youngale@iastate.edu>
2020-01-22 01:56:10 -0600

    Add Example Redis

--- BackEnd/SelecTunes-Backend/Controllers/IndexController.cs
+++ BackEnd/SelecTunes-Backend/Controllers/IndexController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using SelecTunes.Data;
 using SelecTunes.Models;
 
@@ -13,31 +14,43 @@ namespace SelecTunes.Controllers
     {
         private readonly ApplicationContext _context;
 
-        private IDistributedCache _distributedCache;
+        private IDistributedCache _cache;
 
-        public IndexController(ApplicationContext context)
+        public IndexController(ApplicationContext context, IDistributedCache cache)
         {
             _context = context;
+            _cache = cache;
         }
 
         [HttpGet]
         public ActionResult<List<String>> Index() => Ok(new List<String> { "" });
 
         [HttpGet]
-        public ActionResult<String> Ack() {
+        public ActionResult<int> Ack() {
+            Random random = new Random();
             Party p = new Party
             {
                 PartyHost = new HostUser
                 {
                     Id = Guid.NewGuid(),
-                    SpotifyId = "123"
-                }
+                    SpotifyId = random.Next().ToString(),
+                    UserName = random.Next().ToString(),
+                    PhoneNumber = random.Next().ToString(),
+                    IsBanned = false,
+                    IsHost = true
+                },
+                Name = random.Next().ToString(),
+                Id = random.Next()
             };
 
             _context.Parties.Add(p);
             _context.SaveChanges();
 
-            return Ok(p.ToString());
+            String serial = JsonConvert.SerializeObject(p);
+
+            _cache.SetString($"$party:${p.Id}", serial, new DistributedCacheEntryOptions());
+
+            return Ok(p.Id);
         }
     }
 }
