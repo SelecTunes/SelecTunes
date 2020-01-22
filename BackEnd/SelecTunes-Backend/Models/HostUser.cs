c2fa45eb2f01a81c77525e3daf9a92efb5337e80
Alexander Young <youngale@iastate.edu>
2020-01-22 01:54:27 -0600

    Clean Up Usings

--- BackEnd/SelecTunes-Backend/Models/HostUser.cs
+++ BackEnd/SelecTunes-Backend/Models/HostUser.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SelecTunes.Models
 {
     public class HostUser : User
     {
+        [Required]
         public string SpotifyId { get; set; }
     }
 }
