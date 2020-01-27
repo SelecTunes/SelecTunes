fda78a1fb59a5846ec00eb3ca8720b1ce903c880
njtucker <njtucker@iastate.edu>
2020-01-26 19:57:02 -0600

    sockets example

---
+++ Experiments/Nate/websocketsinitial/websocketsinitial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace websocketsinitial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
