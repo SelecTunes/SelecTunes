41a06f275a2534ecb315afae483b26b6ac72ad2e
Joshua Edwards <joshuae1@iastate.edu>
2020-01-27 11:52:03 -0600

    Committing fixes to my code.  Last night I made some code but nothing worked lol

--- Experiments/Josh/app/src/main/java/cs309/selectunes/MainActivity.kt
+++ Experiments/Josh/app/src/main/java/cs309/selectunes/MainActivity.kt
@@ -1,5 +1,6 @@
 package cs309.selectunes
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -20,25 +21,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        startActivity(Intent(this, PhoneNumberExperiment::class.java))
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ Experiments/Josh/app/src/main/java/cs309/selectunes/experiment1.kt
@@ -0,0 +1,19 @@
+package cs309.selectunes
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class experiment1 : AppCompatActivity()
+{
+
+    override fun onCreate(instanceState: Bundle?) {
+        super.onCreate(instanceState)
+        setContentView(R.layout.fragment_home)
+        val textBox = findViewById<TextView>(R.id.text_home)
+        textBox.text = intent.getStringExtra("Phone Number:")
+    }
+
+
+
+}
