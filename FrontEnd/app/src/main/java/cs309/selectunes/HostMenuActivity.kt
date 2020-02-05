1218ddf93e843d7da934b86931903705f8887c77
JackGoldsworth <jackgoldsworth@gmail.com>
2020-02-05 15:35:19 -0600

    Randomly generated string for showing off the session id.

--- FrontEnd/app/src/main/java/cs309/selectunes/HostMenuActivity.kt
+++ FrontEnd/app/src/main/java/cs309/selectunes/HostMenuActivity.kt
@@ -3,16 +3,21 @@ package cs309.selectunes
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HostMenuActivity : AppCompatActivity() {
 
     override fun onCreate(instanceState: Bundle?) {
         super.onCreate(instanceState)
-        setContentView(R.layout.host_menu_main)
-        val guestList = findViewById<Button>(R.id.guestList_id)
-        val songList = findViewById<Button>(R.id.songQueue_id)
-        val backButton = findViewById<Button>(R.id.return_id)
+        setContentView(R.layout.test_host_menu)
+
+        val guestList = findViewById<Button>(R.id.guest_list)
+        val songList = findViewById<Button>(R.id.song_list)
+        val backArrow = findViewById<Button>(R.id.back_arrow)
+        val endParty = findViewById<Button>(R.id.end_party)
+        val moderators = findViewById<Button>(R.id.moderators)
+        val explicit = findViewById<Button>(R.id.explicit)
 
         guestList.setOnClickListener{
             val toGuestList = Intent(this, GuestListActivity::class.java)
@@ -24,8 +29,23 @@ class HostMenuActivity : AppCompatActivity() {
             startActivity(toSongList)
         }
 
-        backButton.setOnClickListener{
+        backArrow.setOnClickListener{
             startActivity(Intent(this, LoginActivity::class.java))
         }
+
+        endParty.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val textView = findViewById<TextView>(R.id.session_id)
+        val source = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+        val randomString = (1..6)
+                .map { i -> kotlin.random.Random.nextInt(0, source.length) }
+                .map(source::get)
+                .joinToString("")
+        textView.text = randomString
     }
 }
