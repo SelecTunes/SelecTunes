5b3e6b0b8d2c1bb7697f0707a2f4669a0413da26
JackGoldsworth <jackgoldsworth@gmail.com>
2020-02-11 20:21:42 -0600

    register and logging in

--- FrontEnd/app/src/main/java/cs309/selectunes/LoginActivity.kt
+++ FrontEnd/app/src/main/java/cs309/selectunes/LoginActivity.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Switch
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import cs309.selectunes.utils.NukeSSLCerts
 
 //https://colorhunt.co/palette/69667
 //https://colorhunt.co/palette/2763
@@ -14,8 +19,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(instanceState: Bundle?) {
         super.onCreate(instanceState)
         setContentView(R.layout.login_menu)
-        val button = findViewById<Button>(R.id.login_button)
+        NukeSSLCerts.nuke()
+        val email = findViewById<TextView>(R.id.login_email)
+        val password = findViewById<TextView>(R.id.login_password)
+        val button = findViewById<Button>(R.id.register_button)
         val darkSlider = findViewById<Switch>(R.id.dark_mode)
+        val register = findViewById<TextView>(R.id.register)
 
         val settings = getSharedPreferences("UserInfo", 0)
         darkSlider.isChecked = settings.getBoolean("dark_mode", false)
@@ -28,18 +37,12 @@ class LoginActivity : AppCompatActivity() {
             editor.apply()
         }
 
+        register.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
         button.setOnClickListener {
-//            val queue = Volley.newRequestQueue(this)
-//            val json = JSONObject()
-//            json.put("phoneNumber", intent.extras?.get("phone") as String)
-//            json.put("username", intent.extras?.get("username") as String)
-//            val authRequest = JsonObjectRequest(Request.Method.POST, "localhost:8080", json, Response.Listener {
-//                println(it)
-//            }, Response.ErrorListener {
-//                println("There was an error with the response.")
-//            })
-//            //queue.add(authRequest)
-            startActivity(Intent(this, ChooseActivity::class.java))
+            login(email.text.toString(), password.text.toString())
         }
     }
 
@@ -49,5 +52,31 @@ class LoginActivity : AppCompatActivity() {
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
+    }
+
+    private fun login(email: String, password: String) {
+        val stringRequest = object: StringRequest(Method.POST, "https://coms-309-jr-2.cs.iastate.edu/api/auth/login", Response.Listener {
+            startActivity(Intent(this, ChooseActivity::class.java))
+            println(it)
+        }, Response.ErrorListener {
+            println("There was an error with the response. Code: ${it.networkResponse.statusCode}")
+        }) {
+            override fun getParams(): Map<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                params["Email"] = email
+                params["Password"] = password
+                params["ConfirmPassword"] = password
+                return params
+            }
+
+            override fun getHeaders(): Map<String, String> {
+                val headers: MutableMap<String, String> = HashMap()
+                headers["Content-Type"] = "application/x-www-form-urlencoded"
+                headers["Accept"] = "application/json, text/json"
+                return headers
+            }
+        }
+        val requestQueue = Volley.newRequestQueue(this)
+        requestQueue.add(stringRequest)
     }
 }
---
+++ FrontEnd/app/src/main/java/cs309/selectunes/RegisterActivity.kt
@@ -0,0 +1,55 @@
+package cs309.selectunes
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Switch
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import cs309.selectunes.utils.NukeSSLCerts
+
+class RegisterActivity: AppCompatActivity() {
+
+    override fun onCreate(instanceState: Bundle?) {
+        super.onCreate(instanceState)
+        setContentView(R.layout.register_menu)
+        NukeSSLCerts.nuke()
+        val email = findViewById<TextView>(R.id.register_email)
+        val password = findViewById<TextView>(R.id.register_password)
+        val confirmPassword = findViewById<TextView>(R.id.register_confirm_password)
+        val button = findViewById<Button>(R.id.register_button)
+
+        button.setOnClickListener {
+            register(email.text.toString(), password.text.toString(), confirmPassword.text.toString())
+        }
+    }
+
+    private fun register(email: String, password: String, passwordConfirmed: String) {
+        val stringRequest = object: StringRequest(Method.POST, "https://coms-309-jr-2.cs.iastate.edu/api/auth/register", Response.Listener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            println(it)
+        }, Response.ErrorListener {
+            println("There was an error with the response. Code: ${it.networkResponse.statusCode}")
+        }) {
+            override fun getParams(): Map<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                params["Email"] = email
+                params["Password"] = password
+                params["ConfirmPassword"] = passwordConfirmed
+                return params
+            }
+
+            override fun getHeaders(): Map<String, String> {
+                val headers: MutableMap<String, String> = HashMap()
+                headers["Content-Type"] = "application/x-www-form-urlencoded"
+                headers["Accept"] = "application/json, text/json"
+                return headers
+            }
+        }
+        val requestQueue = Volley.newRequestQueue(this)
+        requestQueue.add(stringRequest)
+    }
+}
