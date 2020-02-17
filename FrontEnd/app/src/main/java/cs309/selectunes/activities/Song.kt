0aa1f0f8c24efa263fa638164018b8d5f12e745d
Joshua Edwards <joshuae1@iastate.edu>
2020-02-16 21:15:23 -0600

    Put json request into Jacks host menu, still giving 400 error

---
+++ FrontEnd/app/src/main/java/cs309/selectunes/activities/Song.kt
@@ -0,0 +1,10 @@
+package cs309.selectunes.activities
+
+class Song {
+    var songName = ""
+    var artistName = ""
+    var albumUrl = ""
+    var upVotes = 0
+    var downVotes = 0
+    var explicitBool = false
+}
--- FrontEnd/app/src/main/java/cs309/selectunes/activities/SongListActivity.kt
+++ FrontEnd/app/src/main/java/cs309/selectunes/activities/SongListActivity.kt
@@ -22,6 +22,5 @@ class SongListActivity : AppCompatActivity()
         }
 
 
-
     }
 }
