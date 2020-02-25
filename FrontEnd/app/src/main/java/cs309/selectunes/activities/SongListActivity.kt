ba506ae088fa0b8a3b49483922a34a1d82e3c0a8
Joshua Edwards <joshuae1@iastate.edu>
2020-02-24 21:07:57 -0600

    Started Image Caching via Glide third party

--- FrontEnd/app/src/main/java/cs309/selectunes/activities/SongListActivity.kt
+++ FrontEnd/app/src/main/java/cs309/selectunes/activities/SongListActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import cs309.selectunes.R
 
 class SongListActivity : AppCompatActivity()
 {
+    var songs = ArrayList<Song>()
+
     override fun onCreate(instanceState:  Bundle?)
     {
         super.onCreate(instanceState)
@@ -23,4 +27,17 @@ class SongListActivity : AppCompatActivity()
 
 
     }
+
+    fun cacheImages()
+    {
+        val options = RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .override(100,100)
+
+        for(s in songs)
+        {
+
+        }
+    }
+
 }
