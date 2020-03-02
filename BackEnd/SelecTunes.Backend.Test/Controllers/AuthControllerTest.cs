7e63ff79f2096c656a8b7cf397505865bf3146a4
njtucker <njtucker@iastate.edu>
2020-03-02 12:21:27 -0600

    authcontroller is dummy thicc

--- BackEnd/SelecTunes.Backend.Test/Controllers/AuthControllerTest.cs
+++ BackEnd/SelecTunes.Backend.Test/Controllers/AuthControllerTest.cs
@@ -8,10 +8,15 @@ using System.Net.Http;
 using Microsoft.Extensions.Caching.Distributed;
 using SelecTunes.Backend.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity;
+using SelecTunes.Backend.Models;
+using Microsoft.Extensions.Logging;
+using SelecTunes.Backend.Helper;
 
 namespace SelecTunes.Backend.Test.Controllers
 {
-
     internal class AuthControllerTest
     {
         private static readonly DbContextOptions optionBuilder = new DbContextOptionsBuilder<ApplicationContext>().UseInMemoryDatabase(databaseName: "selectunes").Options;
@@ -19,10 +24,19 @@ namespace SelecTunes.Backend.Test.Controllers
         private readonly Mock<ApplicationContext> mockContext = new Mock<ApplicationContext>(optionBuilder);
         private readonly Mock<IDistributedCache> mockCache = new Mock<IDistributedCache>();
         private readonly Mock<IHttpClientFactory> mockFactory = new Mock<IHttpClientFactory>();
+        private readonly Mock<IConfiguration> mockConfig = new Mock<IConfiguration>();
+        private readonly Mock<IOptions<AppSettings>> mockOptions = new Mock<IOptions<AppSettings>>();
+        private readonly Mock<UserManager<User>> mockUserManager = new Mock<UserManager<User>>();
+        private readonly Mock<SignInManager<User>> mockSignInManager = new Mock<SignInManager<User>>();
+        private readonly Mock<ILogger<AuthController>> mockLogger = new Mock<ILogger<AuthController>>();
 
-        private readonly IndexController controller;
+        private readonly Mock<AuthHelper> mockHelper = new Mock<AuthHelper>();
 
-        public AuthControllerTest() => controller = new IndexController(mockContext.Object, mockCache.Object, mockFactory.Object);
+        private readonly Mock<Random> mockRandom = new Mock<Random>();
+
+        private readonly AuthController controller;
+
+        public AuthControllerTest() => controller = new AuthController(mockContext.Object, mockCache.Object, mockFactory.Object, mockConfig.Object, mockOptions.Object, mockUserManager.Object, mockSignInManager.Object, mockLogger.Object, mockHelper.Object);
 
         [Test]
         public void AssertThatInstantiatedControllerIsInstanceOfTheClass()
