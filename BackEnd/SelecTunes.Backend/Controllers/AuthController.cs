1d4b017eefc3e2155d5fd25aca81c3c582ff6f22
njtucker <njtucker@iastate.edu>
2020-03-04 15:20:44 -0600

    fix verbage

--- BackEnd/SelecTunes.Backend/Controllers/AuthController.cs
+++ BackEnd/SelecTunes.Backend/Controllers/AuthController.cs
@@ -259,8 +259,9 @@ namespace SelecTunes.Backend.Controllers
          *
          * 
          * 20/02/2020 - Alexander Young
+         * 04/03/2020 - Nathan Tucker - Fix method verb
          */
-        [HttpGet]
+        [HttpPost]
         [Authorize]
         public async Task<ActionResult<String>> Ban([FromQuery]string email)
         {
