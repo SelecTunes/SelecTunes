06ef0b25dbca7c6a11eb762b295b276035204060
njtucker <njtucker@iastate.edu>
2020-03-06 17:32:03 -0600

    added some test, removed unused stuff

--- BackEnd/SelecTunes.Backend/Models/Auth/AccessAuthToken.cs
+++ BackEnd/SelecTunes.Backend/Models/Auth/AccessAuthToken.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SelecTunes.Backend.Models.Auth
 {
