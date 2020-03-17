ab4e5f1343329d4da3a312644d2957cbcb852c96
njtucker <njtucker@iastate.edu>
2020-03-17 14:16:40 -0500

    models

--- Experiments/Nate/DjangoRewrite/DjangoRewrite/party/models.py
+++ Experiments/Nate/DjangoRewrite/DjangoRewrite/party/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+RewriteUser = get_user_model()
+
+
+class Party(models.Model):
+    join_code = models.CharField(max_length=10)
+    members = models.ForeignKey(RewriteUser, on_delete=models.CASCADE)
--- Experiments/Nate/DjangoRewrite/DjangoRewrite/selectunesauth/models.py
+++ Experiments/Nate/DjangoRewrite/DjangoRewrite/selectunesauth/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from djangorewrite.party.models import Party
+from djangorewrite.song.models import AccessAuthToken
+
+
+class RewriteUser(models.Model):
+    is_banned = models.BooleanField()
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
+    id_of_party = models.IntegerField()
+    access_token = models.ForeignKey(AccessAuthToken, on_delete=models.CASCADE)
+
+
+class InputModel(models.Model):
+    email = models.TextField()
+    password = models.CharField(max_length=255)
+
+
+# class SpotifyIdentity(models.Model):
+#     display_name = models.CharField()
+#     email = models.CharField()
+#     type = models.CharField()
+#     product = models.CharField()
+#
+# class SpotifyLogin(models.Model):
+#     code = models.CharField()
