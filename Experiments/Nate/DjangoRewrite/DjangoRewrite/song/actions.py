f4c3785dac0f49c39d14c160ccfed7555471a30e
njtucker <njtucker@iastate.edu>
2020-03-18 15:20:25 -0500

    actions

---
+++ Experiments/Nate/DjangoRewrite/DjangoRewrite/song/actions.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def search_by_track(track, user):
+    pass
+
+
+def search_by_artist(artist, user):
+    pass
+
+
+def add_to_queue(song):
+    pass
+
+
+def remove_from_queue(song):
+    pass
