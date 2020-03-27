ffa6df88b403e5f645785be1a66dc241158af705
njtucker <njtucker@iastate.edu>
2020-03-26 22:03:44 -0500

    migrations

---
+++ BackEnd/SelecTunes.Backend/Migrations/20200327030332_KickAndBanUsers.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SelecTunes.Backend.Migrations
+{
+    public partial class KickAndBanUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PartyId1",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Strikes",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_PartyId1",
+                table: "AspNetUsers",
+                column: "PartyId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Parties_PartyId1",
+                table: "AspNetUsers",
+                column: "PartyId1",
+                principalTable: "Parties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Parties_PartyId1",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_PartyId1",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PartyId1",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Strikes",
+                table: "AspNetUsers");
+        }
+    }
+}
