e9ebfc90156f94d51d91530a195f8f72974a4d55
Alexander Young <youngale@iastate.edu>
2020-03-30 20:35:02 -0500

    Remove from Queue websocket

--- BackEnd/SelecTunes.Backend/Helper/Hubs/QueueHub.cs
+++ BackEnd/SelecTunes.Backend/Helper/Hubs/QueueHub.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using SelecTunes.Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SelecTunes.Backend.Helper.Hubs
 {
     public class QueueHub : Hub
     {
+        private readonly IDistributedCache _cache;
+        public QueueHub(IDistributedCache cache)
+        {
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
 
         public async Task JoinQueue(string queueName)
         {
@@ -32,10 +43,50 @@ namespace SelecTunes.Backend.Helper.Hubs
             await Clients.All.SendAsync("ReceiveMoveSongToFront", SpotifyID).ConfigureAwait(false);
         }
 
-        public async Task RemoveSong(string SpotifyID)
+        public async Task RemoveSong(string spotifyId, string partyId)
         {
-            // TODO Add song to front of queue
-            await Clients.All.SendAsync("ReceiveRemoveSong", SpotifyID).ConfigureAwait(false);
+            byte[] queue = await _cache.GetAsync($"$queue:${partyId}").ConfigureAwait(false);
+            if (queue == null)
+            {
+                return;
+            }
+
+            Queue<Song> q = JsonConvert.DeserializeObject<Queue<Song>>(Encoding.UTF8.GetString(queue));
+
+            Console.WriteLine(JsonConvert.SerializeObject(q));
+
+            List<Song> list = BuildSongList(q);
+
+            list.RemoveAll(song => song.Id == spotifyId);
+            
+            Console.WriteLine(JsonConvert.SerializeObject(list));
+
+            q = BuildSongQueue(list);
+
+            await _cache.SetStringAsync($"$queue:${partyId}", JsonConvert.SerializeObject(q)).ConfigureAwait(false);
+
+            await Clients.All.SendAsync("ReceiveRemoveSong", spotifyId).ConfigureAwait(false);
+        }
+
+        private static List<Song> BuildSongList(Queue<Song> queue)
+        {
+            List<Song> list = new List<Song>();
+
+            while (queue.Count != 0)
+            {
+                list.Add(queue.Dequeue());
+            }
+
+            return list;
+        }
+
+        private static Queue<Song> BuildSongQueue(List<Song> list)
+        {
+            Queue<Song> queue = new Queue<Song>();
+
+            list.ForEach(queue.Enqueue);
+
+            return queue;
         }
     }
 }
