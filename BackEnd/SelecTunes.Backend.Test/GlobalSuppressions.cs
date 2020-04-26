94858bfccdb49ca2f55ea9a048caa8264fc3515d
Alexander Young <youngale@iastate.edu>
2020-04-26 16:52:02 -0500

    Suppress incorrect warnings
    Signed-off-by: Alexander Young <youngale@iastate.edu>

---
+++ BackEnd/SelecTunes.Backend.Test/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "<Pending>", Scope = "member", Target = "~M:SelecTunes.Backend.Test.Controllers.FakeSignInManager.#ctor")]
+[assembly: SuppressMessage("Build", "CA1812:AuthControllerTest is an internal class that is apparently never instantiated. If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic).", Justification = "<Pending>", Scope = "type", Target = "~T:SelecTunes.Backend.Test.Controllers.AuthControllerTest")]
+[assembly: SuppressMessage("Build", "CA1812:IndexControllerTest is an internal class that is apparently never instantiated. If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic).", Justification = "<Pending>", Scope = "type", Target = "~T:SelecTunes.Backend.Test.Controllers.IndexControllerTest")]
+[assembly: SuppressMessage("Build", "CA1812:PartyControllerTest is an internal class that is apparently never instantiated. If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic).", Justification = "<Pending>", Scope = "type", Target = "~T:SelecTunes.Backend.Test.Controllers.PartyControllerTest")]
+[assembly: SuppressMessage("Build", "CA1812:SongControllerTest is an internal class that is apparently never instantiated. If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic).", Justification = "<Pending>", Scope = "type", Target = "~T:SelecTunes.Backend.Test.Controllers.SongControllerTest")]
+[assembly: SuppressMessage("Build", "CA1812:QueueHubHelperTest is an internal class that is apparently never instantiated. If so, remove the code from the assembly. If this class is intended to contain only static members, make it static (Shared in Visual Basic).", Justification = "<Pending>", Scope = "type", Target = "~T:SelecTunes.Test.Controllers.QueueHubHelperTest")]
